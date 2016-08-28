2fc11a64cc95edff931b04c87f034057cae40f00
Scott McLeod <halcyonblue@gmail.com>
2016-08-28 16:16:00 -0400

    Adding CDs

--- c3.lisp
+++ c3.lisp
@@ -1,2 +1,35 @@
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
+
+(make-cd "Roses" "Kathy Mattea" 7 t)
+
+(defvar *db* nil)
+
+(defun add-record (cd) (push cd *db*))
+
+(add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+(add-record (make-cd "Fungus" "John Smith" 3 t))
+(add-record (make-cd "No More Stu" "Stuart Halloway" 5 t))
+(add-record (make-cd "Mowing His Lawn" "Rich Hickey" 2 t))
+(add-record (make-cd "Totally Insane" "Zach Tellman" 1 t))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
